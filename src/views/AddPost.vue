c36645970e590473545a66d1559051a2acca60fa
Diki Agung Mustaqim <agungmustaqimdiki@gmail.com>
2021-09-20 08:21:22 +0700

    integrated with API

---
+++ src/views/AddPost.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Add post page</h1>
+</template>
+
+<script>
+export default {
+  name: "Add post",
+  components: {},
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,41 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row>
+      <v-col sm="4" class="pa-3" v-for="post in posts" :key="post._id">
+        <v-card class="pa-1">
+          <v-img height="250" :src="`/${post.image}`"></v-img>
+          <v-btn class="ml-4 mt-3" small outlined color="indigo">
+            {{ post.category }}
+          </v-btn>
+
+          <v-card-title class="headline">
+            {{ post.title }}
+          </v-card-title>
+
+          <v-card-text class="py-0">
+            <p>{{ post.content.substring(0, 100) + "..." }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import Api from "../api";
+
 export default {
   name: "Home",
 
-  components: {},
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  async created() {
+    this.posts = await Api.getAllPost();
+    console.log(this.posts);
+  },
 };
 </script>
